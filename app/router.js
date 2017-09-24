e22e30663ebea74bbc72b32bc8d92f6691278eb3
Njoroge Mungai <njorogemungai1980@gmail.com>
2017-09-24 18:03:45 +0300

    integrate missing files

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('questions', {
+    path: '/questions/:questions-id'
+  });
 });
 
 export default Router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,14 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('questions');
   },
+  actions: {
+    saveQuestion(details) {
+      var newQuestion = this.store.createRecord('question', details);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
+
+    // contributeQuestion() {
+
+  }
 });
